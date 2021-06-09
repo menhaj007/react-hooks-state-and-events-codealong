8ff6f3e16660a36dfbf4b101e45bcc11b29be242
menhaj <menhaj007@yahoo.com>
2021-06-08 21:38:34 -0400

    Completed

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,19 @@ import React, {useState} from "react";
 import Toggle from "./Toggle";
 
 function App() {
-  const [isOn, setOnOff] = useState(false);
+  // const [isOn, setOnOff] = useState(false);
 
-  function swtichOnOff() {
-    setOnOff( (prev) =>  prev = !prev);
+  // function swtichOnOff() {
+  //   setOnOff( (prev) =>  prev = !prev);
 
-  }
+  // }
  
 
   return (
     <div>
       <h3>Toggle</h3>
-      <Toggle isOn={isOn} setOnOff={swtichOnOff}/>
+      {/* <Toggle isOn={isOn} setOnOff={swtichOnOff}/> */}
+      <Toggle />
     </div>
   );
 }
--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 
-function Toggle({isOn, setOnOff}) {
-  let status = "";
-  return <button onClick={() => (isOn == true? setOnOff(isOn = false): setOnOff(isOn = true))}> {status }{isOn == true? status="ON": status="OFF"}</button>;
-}
 
+// function Toggle({isOn, setOnOff}) {
+//   let status = "";
+//   return <button onClick={() => (isOn == true? setOnOff(isOn = false): setOnOff(isOn = true))}> {status }{isOn == true? status="ON": status="OFF"}</button>;
+// }
+
+// export default Toggle;
+
+function Toggle() {
+  const [isOn, setIsOn] = useState(false);
+
+  function handleClick() {
+    setIsOn((isOn) => !isOn);
+  }
+
+  const color = isOn ? "red" : "white";
+
+  return (
+    <button style={{ background: color }} onClick={handleClick}>
+      {isOn ? "ON" : "OFF"}
+    </button>
+  );
+} 
 export default Toggle;
