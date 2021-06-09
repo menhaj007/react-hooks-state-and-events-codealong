5c88a9fc1a745614c0272f206405effa3924153e
menhaj <menhaj007@yahoo.com>
2021-06-08 21:29:06 -0400

    commit tree f874854142e2bac65df2abed4b763cbeadcaa899

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
+
 import Toggle from "./Toggle";
 
 function App() {
+  const [isOn, setOnOff] = useState(false);
+
+  function swtichOnOff() {
+    setOnOff( (prev) =>  prev = !prev);
+
+  }
+ 
+
   return (
     <div>
       <h3>Toggle</h3>
-      <Toggle />
+      <Toggle isOn={isOn} setOnOff={swtichOnOff}/>
     </div>
   );
 }
--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,8 @@
 import React from "react";
 
-function Toggle() {
-  return <button>OFF</button>;
+function Toggle({isOn, setOnOff}) {
+  let status = "";
+  return <button onClick={() => (isOn == true? setOnOff(isOn = false): setOnOff(isOn = true))}> {status }{isOn == true? status="ON": status="OFF"}</button>;
 }
 
 export default Toggle;
